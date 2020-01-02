4351617cc9bb168b302032567e0a1298a7b89756
31701072 计算机1703 黄晨觉 <1097280886@qq.com>
2020-01-03 00:35:30 +0800

    31701072_test

---
+++ microc/test_pmdtassign.c
@@ -0,0 +1,17 @@
+void main(){
+	int i;
+	i = 1;
+	i += 1;
+	print i;
+	i -= 1;
+	print i;
+	int j;
+	j = 2;
+	i *= 2;
+	print i;
+    i = 4;
+	i /= j;
+	print i;
+	i %= i;
+	print i;
+}
---
+++ microc/test_sanmu.c
@@ -0,0 +1,7 @@
+void main(){
+	int i; 
+	int j; 
+	i=2; 
+	j=55; 
+	(j>i)?print j:print i;
+}
